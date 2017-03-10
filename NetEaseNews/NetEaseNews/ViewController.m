5b60d5145c8a50272d6fe30f52415dcf98fd0d52
appleFang <1414728727@qq.com>
2017-03-10 20:08:20 +0800

    collectionView布局完成

--- NetEaseNews/NetEaseNews/ViewController.m
+++ NetEaseNews/NetEaseNews/ViewController.m
@@ -12,13 +12,16 @@
 #define scrollLabelWidth 80
 #define scrollLabelHeight 44
 
-@interface ViewController ()
+@interface ViewController ()<UICollectionViewDataSource>
 
 @property (weak, nonatomic) IBOutlet UIScrollView *newsScrollView;
 
 @property (weak, nonatomic) IBOutlet UICollectionView *newsCollection;
 
 @property (nonatomic, strong) NSArray<ChannelModel *> *channelData;
+
+@property (weak, nonatomic) IBOutlet UICollectionViewFlowLayout *flowLayout;
+
 @end
 
 @implementation ViewController
@@ -29,6 +32,8 @@
     [self loadData];
     
     [self reloadNewsScrollView];
+    
+    [self reloadNewsCollection];
     
     self.automaticallyAdjustsScrollViewInsets = NO;
 }
@@ -54,6 +59,37 @@
     self.newsScrollView.showsHorizontalScrollIndicator = NO;
 }
 
+- (void)reloadNewsCollection{
+    
+    self.newsCollection.dataSource = self;
+    
+    self.flowLayout.itemSize = CGSizeMake(self.newsCollection.bounds.size.width, self.newsCollection.bounds.size.height);
+    
+    self.flowLayout.minimumLineSpacing = 0;
+    self.flowLayout.minimumInteritemSpacing = 0;
+    self.newsCollection.bounces = NO;
+    
+    self.newsCollection.showsVerticalScrollIndicator = NO;
+    self.newsCollection.showsHorizontalScrollIndicator = NO;
+    
+    self.flowLayout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    
+    self.newsCollection.pagingEnabled = YES;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    
+    return self.channelData.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"newsCell" forIndexPath:indexPath];
+    
+    cell.backgroundColor = [UIColor colorWithRed:arc4random_uniform(256)/255.0 green:arc4random_uniform(256)/255.0 blue:arc4random_uniform(256)/255.0 alpha:1];
+    
+    return cell;
+}
 - (void)loadData{
     
     self.channelData = [ChannelModel createChannelModel];
