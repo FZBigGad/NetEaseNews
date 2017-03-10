778612706ddc161b2098e49892dd1c296eda4d36
appleFang <1414728727@qq.com>
2017-03-10 18:50:56 +0800

    项目准备

---
+++ NetEaseNews/NetEaseNews/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  NetEaseNews
+//
+//  Created by fangzhengwei on 2017/3/10.
+//  Copyright © 2017年 fangzhengwei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   
+
+
+}
+
+
+@end
---
+++ NetEaseNews/NetEaseNews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NetEaseNews
+//
+//  Created by fangzhengwei on 2017/3/10.
+//  Copyright © 2017年 fangzhengwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
